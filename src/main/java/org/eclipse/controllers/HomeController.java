d4a5eb24355f77044fbb7d97f5adc70626576bbf
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2023-01-13 15:16:15 +0100

    on commence à bosser avec une API TTS : VoiceRSS

--- src/main/java/org/eclipse/controllers/HomeController.java
+++ src/main/java/org/eclipse/controllers/HomeController.java
@@ -1,13 +1,23 @@
 package org.eclipse.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+	@Value("${apiKey}")
+	private String apiKey;
 	
 	@GetMapping({"/tokitalk", "/"})
 	public String Home() {
 		return "Home";
+	}
+	
+	@PostMapping("/blabla")
+	public String TTS(@RequestParam(name="phrase") String phrase) {
+		return ("http://api.voicerss.org/?key=" + apiKey + "&hl=fr-fr&v=Iva&src=" + phrase);
 	}
 }
