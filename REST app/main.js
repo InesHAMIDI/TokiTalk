940b40ec42e281227e15d3bf4e468ffa8002cf69
Ines HAMIDI <ineshamid@gmail.com>
2023-01-17 16:42:49 +0100

    début des requetes

--- REST app/main.js
+++ REST app/main.js
@@ -1,1 +1,10 @@
 //http://api.voicerss.org/?key=7752ff267a4547ba914bce6d0ff0a23f&hl=fr-fr&v=Iva&src=TEXT
+
+function lireAudio(text){
+    //on vérifie que la string envoyée est bonne
+    //si bonne : OK
+    //sinon : retour
+    if(text == "" || text == " "){
+        return;
+    }
+}
