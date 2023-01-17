224236037ad205137deea56af49750e880ac74aa
Ines HAMIDI <ineshamid@gmail.com>
2023-01-17 11:11:57 +0100

    on essaie de suivre les cours de Achref mais va surement falloir reprendre ce soir doucment

---
+++ REST app/TokiTalk/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/main.css'
+
+createApp(App).mount('#app')
+
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import "@fortawesome/fontawesome-free/css/all.css"
