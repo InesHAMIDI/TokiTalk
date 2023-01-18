f9b42a7c04968b5b55bbc254460a7852b18dd7b6
Ines HAMIDI <ineshamid@gmail.com>
2023-01-18 12:34:09 +0100

    le import fait qu'on trouve plus les functions ??

--- REST app/main.js
+++ REST app/main.js
@@ -1,25 +1,29 @@
-function recupererTextEcrit(){
+import VoiceRSSWebApi from 'voice-rss-api.js';
+
+function recupererTextEcrit() {
     let text = document.getElementById("texteALire").value;
     lireAudio(text);
 }
 
-function lireAudio(text){
+function lireAudio(text) {
     console.log(text);
     VoiceRSS.getAudio(text)
-    .then(audio => new Audio(audio).play())
-    .catch(error => console.log(error));
- }
+        .then(audio => new Audio(audio).play())
+        .catch(error => console.log(error));
+}
 
- import VoiceRSSWebApi from './voice-rss-api';
+function loadData() {
+    let key = "7752ff267a4547ba914bce6d0ff0a23f";
 
- let key = "7752ff267a4547ba914bce6d0ff0a23f";
+    const VoiceRSS = new VoiceRSSWebApi();
+    VoiceRSS.setApiKey(key);
+    VoiceRSS.setLanguage('fr-fr');
+    VoiceRSS.setSpeechRate(0);
+    VoiceRSS.setAudioCodec('auto');
+    VoiceRSS.setAudioFormat('44khz_16bit_stereo');
+    VoiceRSS.setSSML(false);
+    VoiceRSS.setB64(false);
+    VoiceRSS.setVoice('Iva');
+}
 
-const VoiceRSS = new VoiceRSSWebApi();
-VoiceRSS.setApiKey(key);
-VoiceRSS.setLanguage('fr-fr');
-VoiceRSS.setSpeechRate(0);
-VoiceRSS.setAudioCodec('auto');
-VoiceRSS.setAudioFormat('44khz_16bit_stereo');
-VoiceRSS.setSSML(false);
-VoiceRSS.setB64(false);
-VoiceRSS.setVoice('Iva');
+
