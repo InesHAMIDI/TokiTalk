a4ba3094db94f5c434aa62daf5fc49aaf70b26f1
Ines HAMIDI <ineshamid@gmail.com>
2023-01-18 12:20:29 +0100

    bizarre, maintenant il connait plus mes fonctions js

--- REST app/main.js
+++ REST app/main.js
@@ -1,14 +1,25 @@
-import VoiceRSSWebApi from './voice-rss-api';let key = "7752ff267a4547ba914bce6d0ff0a23f"
 function recupererTextEcrit(){
     let text = document.getElementById("texteALire").value;
     lireAudio(text);
 }
 
-function recupererBoutonClicke(){
-    let text;
-    lireAudio(text);
-}
-
 function lireAudio(text){
     console.log(text);
+    VoiceRSS.getAudio(text)
+    .then(audio => new Audio(audio).play())
+    .catch(error => console.log(error));
  }
+
+ import VoiceRSSWebApi from './voice-rss-api';
+
+ let key = "7752ff267a4547ba914bce6d0ff0a23f";
+
+const VoiceRSS = new VoiceRSSWebApi();
+VoiceRSS.setApiKey(key);
+VoiceRSS.setLanguage('fr-fr');
+VoiceRSS.setSpeechRate(0);
+VoiceRSS.setAudioCodec('auto');
+VoiceRSS.setAudioFormat('44khz_16bit_stereo');
+VoiceRSS.setSSML(false);
+VoiceRSS.setB64(false);
+VoiceRSS.setVoice('Iva');
