039424b7cadf5b108edb8e5508164fac37574c0c
Ines HAMIDI <ineshamid@gmail.com>
2023-01-19 10:42:54 +0100

    on essaie de bosser avec l'api

--- REST app/main.js
+++ REST app/main.js
@@ -1,17 +1,34 @@
-import VoiceRSSWebApi from 'voice-rss-api.js';
+"use strict";
+import VoiceRSSWebApi from "./voice-rss-api.js";
+
+const bvalider = document.getElementById("b-valider");
+bvalider.addEventListener("click", recupererTextEcrit)
 
 function recupererTextEcrit() {
     let text = document.getElementById("texteALire").value;
     lireAudio(text);
 }
 
-function lireAudio(text) {
+const boui = document.getElementById("b-oui");
+const bnon = document.getElementById("b-non");
+const bjsp = document.getElementById("b-jsp");
+
+boui.addEventListener("click", lireAudio("oui"))
+bnon.addEventListener("click", lireAudio("non"))
+bjsp.addEventListener("click", lireAudio("je ne sais pas"))
+
+async function lireAudio(text) {
     console.log(text);
-    VoiceRSS.getAudio(text)
-        .then(audio => new Audio(audio).play())
-        .catch(error => console.log(error));
+    try {
+        await(VoiceRSS.getAudio(text))
+        audio => new Audio(audio).play()
+    }
+    catch {
+        error => console.log(error);
+    }
 }
 
+document.addEventListener("load", loadData)
 function loadData() {
     let key = "7752ff267a4547ba914bce6d0ff0a23f";
 
