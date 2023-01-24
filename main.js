3d0b99e2b7a34a81abb5007983f51c94db01151a
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2023-01-24 15:04:41 +0100

    quelques petits bugs et on suit le tuto

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+  
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    }
   })
-
   win.loadFile('index.html')
 }
 
@@ -14,37 +17,6 @@ app.whenReady().then(() => {
   createWindow()
 })
 
-
-//code de TokiTalk
-const bvalider = document.getElementById("b-valider");
-const boui = document.getElementById("b-oui");
-const bnon = document.getElementById("b-non");
-const bjsp = document.getElementById("b-jsp");
-
-bvalider.addEventListener("click", () => { recupererTextEcrit() })
-boui.addEventListener("click", () => { lireAudio("oui"); })
-bnon.addEventListener("click", () => { lireAudio("non"); })
-bjsp.addEventListener("click", () => { lireAudio("je ne sais pas"); })
-
-
-function recupererTextEcrit() {
-    let text = document.getElementById("texteALire").value;
-    lireAudio(text);
-}
-
-function lireAudio(text) {
-    console.log(text);
-
-    var msg = new SpeechSynthesisUtterance();
-    var voices = window.speechSynthesis.getVoices();
-    msg.voice = voices[10]; 
-    msg.volume = 1; // From 0 to 1
-    msg.rate = 1; // From 0.1 to 10
-    msg.pitch = 0; // From 0 to 2
-    msg.lang = 'fr';
-
-    msg.text = text;
-    window.speechSynthesis.speak(msg);
-
-}
-
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
