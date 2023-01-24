f648d4bd3b4bbf0e5148334338cdf03060764935
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2023-01-24 15:26:28 +0100

    tout marche mais l'image veut pas

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 //on crée la fenêtre electron
 const { app, BrowserWindow } = require('electron')
+const path = require('path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-  
+    width: 300,
+    height: 500,
+    icon: './favicon.svg',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     }
-  })
+  });
   win.loadFile('index.html')
-}
+};
 
 app.whenReady().then(() => {
-  createWindow()
-})
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
